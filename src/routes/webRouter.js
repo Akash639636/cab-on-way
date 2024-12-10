d31790bfcc8ef6ace7ecbad3cab7ecf714fe6af5
Yash Kumar Jha <153628338+Akash639636@users.noreply.github.com>
2024-12-10 22:26:46 +0530

    add webrouter

--- src/routes/webRouter.js
+++ src/routes/webRouter.js
@@ -1,6 +1,26 @@
 const {app} = require("../app");
 
 
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/privacy-policy", (req, res) => {
+    res.render("privacyPolicy");
+});
+
+app.get("/about-us", (req, res) => {
+    res.render("aboutUs");
+});
+
+app.get("/terms-and-condition", (req, res) => {
+    res.render("termsAndConditions");
+});
+
+app.get("/refund-policy", (req, res) => {
+    res.render("refundPolicy");
+});
+
 app.get("/verify-payment", (req, res) => {
     res.render("verifyPayment");
 });
